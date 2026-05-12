a3ccfe8248fe26dae65085b45528965e5f7e9752
David Linares Fdez. <234041294+David-Linares-Fdez@users.noreply.github.com>
2026-05-12 16:06:57 +0100

    Implementación de guardado de escena
    Prácticamente funcional.
    Para comprobar que funcione, hay que empezar desde la escena 0.
    Queda por programar que cuando comiences, el botón de "reanudar" esté desactivado.
    Metidos los SFX de la UI

---
+++ Assets/Script/Menu/Scenes_Buttons.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Scenes_Buttons : MonoBehaviour
+{
+    int current;
+   
+
+    public void MainMenu()
+    {
+        current = SceneManager.GetActiveScene().buildIndex;
+        Mantener_Scene.Instance.keptScene = current;
+        PlayerPrefs.SetInt("LastScene", current);
+        SceneManager.LoadScene(0);
+    }
+
+    public void ReturnToScene()
+    {
+        int lastScene = PlayerPrefs.GetInt("LastScene", 0);
+        SceneManager.LoadScene(lastScene);
+    }
+
+}
