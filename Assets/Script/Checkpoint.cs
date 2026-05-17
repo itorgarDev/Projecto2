ea70a3129572462da8f340965f0e76b0a35887c0
David Linares Fdez. <234041294+David-Linares-Fdez@users.noreply.github.com>
2026-05-17 13:54:42 +0100

    Cambios código
    - Sistema del mapa implementado con placeholders
    - Sistema de guardado de checkpoints a medio hacer

--- Assets/Script/Checkpoint.cs
+++ Assets/Script/Checkpoint.cs
@@ -2,11 +2,23 @@ using UnityEngine;
 
 public class Checkpoint : MonoBehaviour
 {
+    public int numeroCkeckpoint;
+
+    private void Awake()
+    {
+
+        RespawnSystem.Checkpoints[numeroCkeckpoint] = transform.position;
+    }
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
             RespawnSystem.LastCheckpointPos = transform.position;
+            RespawnSystem.CurrentCheckpointIndex = numeroCkeckpoint;
+
+            PlayerPrefs.SetInt("SavedCheckpoint", numeroCkeckpoint);
+            PlayerPrefs.Save();
         }
     }
 }
--- Assets/Script/Menu/Menu/Checkpoint_System.cs
+++ Assets/Script/Menu/Menu/Checkpoint_System.cs
@@ -25,29 +25,32 @@ public class Checkpoint_System : MonoBehaviour
     public void CheckpointPoint()
     {
         var playerGet = GetPlayer();
-        playerGet.transform.position = RespawnSystem.LastCheckpointPos + new Vector3(2, 0, 0);
+
+        Vector3 pos = RespawnSystem.GetCheckpointPosition();
+
+        playerGet.transform.position = pos + new Vector3(2, 0, 0);
+
         Rigidbody rb = player.GetComponent<Rigidbody>();
         rb.velocity = Vector3.zero;
         rb.angularVelocity = Vector3.zero;
-        Debug.Log("Menu cerrado");
 
         player.ClosePauseMenu();
-
     }
 
- /*   public void MainMenu()
-    {
-        keptScene=SceneManager.GetActiveScene().buildIndex;
-        PlayerPrefs.SetInt("LastScene", keptScene);
-        SceneManager.LoadScene(0);
-    }
 
-    public void ReturnToScene(int currentScene)
-    {
-        int lastScene = PlayerPrefs.GetInt("LastScene", 0);
+    /*   public void MainMenu()
+       {
+           keptScene=SceneManager.GetActiveScene().buildIndex;
+           PlayerPrefs.SetInt("LastScene", keptScene);
+           SceneManager.LoadScene(0);
+       }
 
-        SceneManager.LoadScene(currentScene);
-    }
- */
+       public void ReturnToScene(int currentScene)
+       {
+           int lastScene = PlayerPrefs.GetInt("LastScene", 0);
+
+           SceneManager.LoadScene(currentScene);
+       }
+    */
 
 }
