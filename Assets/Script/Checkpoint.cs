0eec623f5fe42397e11c662a6847ffcdbbcdfc0c
David Linares Fdez. <234041294+David-Linares-Fdez@users.noreply.github.com>
2026-05-18 15:46:48 +0100

    Salvado de posición de checkpoint

--- Assets/Script/Checkpoint.cs
+++ Assets/Script/Checkpoint.cs
@@ -15,10 +15,17 @@ public class Checkpoint : MonoBehaviour
         if (other.CompareTag("Player"))
         {
             RespawnSystem.LastCheckpointPos = transform.position;
-            RespawnSystem.CurrentCheckpointIndex = numeroCkeckpoint;
+          //  RespawnSystem.CurrentCheckpointIndex = numeroCkeckpoint;
 
             PlayerPrefs.SetInt("SavedCheckpoint", numeroCkeckpoint);
+            PlayerPrefs.SetFloat("CP_X", transform.position.x);
+            PlayerPrefs.SetFloat("CP_Y", transform.position.y);
+            PlayerPrefs.SetFloat("CP_Z", transform.position.z);
             PlayerPrefs.Save();
+
+            Debug.Log("Checkpoint " + numeroCkeckpoint + " activado. Pos = " + transform.position);
         }
     }
+    
 }
+
