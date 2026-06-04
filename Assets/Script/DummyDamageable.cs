e7f23f0286999d8681dcda5f43c0c96c206fb3e9
ItorgarDev <itorgartech@gmail.com>
2026-06-04 20:43:23 +0200

    actualizacion

--- Assets/Script/DummyDamageable.cs
+++ Assets/Script/DummyDamageable.cs
@@ -5,8 +5,9 @@ public class DummyDamageable : MonoBehaviour, IDamageable
     [Header("Configuración de Vida")]
     [SerializeField] private float maxHealth = 100f;
     private float currentHealth;
+    public GameObject key;
 
-    // --- ESTO ES OBLIGATORIO: Permite al HUDController leer la vida en el Update ---
+    
     public float CurrentHealth => currentHealth;
 
     [Header("Configuración del HUD")]
@@ -26,10 +27,10 @@ public class DummyDamageable : MonoBehaviour, IDamageable
 
         if (showBarInHUD && HUDController.Instance != null && currentHealth > 0)
         {
-            // --- REVISA ESTA LÍNEA: Debe llamar a TrackDummy pasándole (this, nombre, vidaMáxima) ---
+            
             HUDController.Instance.TrackDummy(this, objectName, maxHealth);
 
-            // Temporizador para ocultar la barra si el jugador se aleja o no le pega más
+            
             CancelInvoke(nameof(HideBarDueToInactivity));
             Invoke(nameof(HideBarDueToInactivity), 4f);
         }
@@ -42,7 +43,7 @@ public class DummyDamageable : MonoBehaviour, IDamageable
 
     private void HideBarDueToInactivity()
     {
-        // --- REVISA ESTA LÍNEA: Debe llamar a UntrackDummy ---
+       
         if (HUDController.Instance != null)
         {
             HUDController.Instance.UntrackDummy();
@@ -51,12 +52,12 @@ public class DummyDamageable : MonoBehaviour, IDamageable
 
     private void Die()
     {
-        // --- REVISA ESTA LÍNEA: Debe llamar a UntrackDummy ---
+      
         if (HUDController.Instance != null)
         {
             HUDController.Instance.UntrackDummy();
         }
-
+        key.SetActive(true);
         Destroy(gameObject);
     }
 }
